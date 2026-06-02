9e0f45a608ff677682956061b136e41dd2e2200b
N○VA STUDI●S <elgatovolador3500@gmail.com>
2026-06-01 23:30:31 -0700

    Create items.js

---
+++ items.js
@@ -0,0 +1,25 @@
+const ITEMS = [
+  {
+    id: "2048",
+    name: "2048",
+    description: "El clásico juego de números.",
+    image: "https://play2048.co/meta/og-image.png",
+    url: "https://play2048.co/",
+    category: "Puzzle",
+    featured: true,
+    type: "game"
+  },
+
+  {
+    id: "tetris",
+    name: "Tetris Online",
+    description: "Juega al legendario Tetris.",
+    image: "https://tetris.com/sites/default/files/tetris-home-module-art_4.png",
+    url: "https://tetris.com/play-tetris",
+    category: "Clásicos",
+    featured: true,
+    type: "game"
+  }
+
+  // Resto de tus juegos...
+];
